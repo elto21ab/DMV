8f2be115f22f5224256e2cbcc3781eb06b2d1941
BastianMK <bas-mk@live.dk>
2025-02-13 22:00:03 +0100

    ,

--- sql/FactListing_Create_Table.sql
+++ sql/FactListing_Create_Table.sql
@@ -1,36 +1,15 @@
-CREATE TABLE fact.factlistings (
-    listing_id INTEGER PRIMARY KEY,
-    accommodation_id INTEGER REFERENCES dim.dimaccommodation(accommodation_id),
-    host_id INTEGER REFERENCES dim.dimhost(host_id),
-    review_id INTEGER REFERENCES dim.dimreview(review_id),
+CREATE TABLE fact.FactListing (
+    accommodation_key INTEGER REFERENCES dim.DimAccommodation(accommodation_key),
+    host_key INTEGER REFERENCES dim.DimHost(host_key),
+    review_key INTEGER REFERENCES dim.DimReviews(review_key),
+    listing_id VARCHAR(50),
     price_DKK DECIMAL,
     instant_bookable BOOLEAN,
-    avg_sentiment_score DECIMAL,
-    booking_count INTEGER,
-    lifespan_years DECIMAL,
-    booking_freq_in_lifespan DECIMAL,
+    avg_sentiment DECIMAL,
+    active_period_years DECIMAL,
+    number_of_bookings INTEGER,
     review_scores_rating DECIMAL,
     number_of_reviews INTEGER,
     reviews_per_month DECIMAL,
     yearly_reviews INTEGER
-);
-
-INSERT INTO fact.FactListings
-SELECT 
-    l.id as listing_id,
-    lam.accommodation_id,
-    l.host_id,
-    lrm.review_id,
-    l.price as price_DKK,
-    l.instant_bookable,
-    l.avg_sentiment_score,
-    l.booking_count,
-    l.lifespan_years,
-    l.booking_freq_in_lifespan,
-    l.review_scores_rating,
-    l.number_of_reviews,
-    l.reviews_per_month,
-    l.yearly_reviews
-FROM public.listings l
-JOIN dim.listing_accommodation_map lam ON l.id = lam.listing_id
-JOIN dim.listing_review_map lrm ON l.id = lrm.listing_id;
+) 
