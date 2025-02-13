4a731f9ef468fb0263c0ffd8aabe44547da6d7ae
BastianMK <bas-mk@live.dk>
2025-02-13 15:30:39 +0100

    views updated

--- sql/DimAccommodation_View.sql
+++ sql/DimAccommodation_View.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE VIEW dim.DimAccommodation AS
 SELECT DISTINCT
-    ROW_NUMBER() OVER (ORDER BY room_type, property_type, neighborhood, latitude, longitude) as accommodation_id,
+    ROW_NUMBER() OVER (ORDER BY room_type, property_type, host_neighbourhood, latitude, longitude) as accommodation_id,
     room_type,
     property_type,
     amenities_count,
     accommodates,
-    baths,
+    bathrooms,
     beds,
     bedrooms,
-    neighborhood,
+    host_neighbourhood as neighborhood,
     latitude,
     longitude,
-    distance_to_center
-FROM public.listings
+    distance_to_center_km
+FROM public.listings  
